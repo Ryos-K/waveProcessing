3735fc8c19edb332a36d27e0f5cfaa6c0b40e9fb
ry05k2ulv <ryosuke.10117@gmail.com>
2023-08-29 09:26:12 +0900

    implement showInfo() function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,9 @@
 import waveprocessor.Mono16Processor
 
 fun main(args: Array<String>) {
-    println("Hello World!")
-
-    val waveProcessor = Mono16Processor()
-    waveProcessor
-        .read("src/main/resources/guitar_A4.wav")
-        ?.write("src/main/resources/hoge.wav")
+	val waveProcessor = Mono16Processor()
+	waveProcessor
+		.read("src/main/resources/guitar_A4.wav")
+		?.showInfo()
+		?.write("src/main/resources/hoge.wav")
 }
--- src/main/kotlin/waveprocessor/Mono16Processor.kt
+++ src/main/kotlin/waveprocessor/Mono16Processor.kt
@@ -2,68 +2,85 @@ package waveprocessor
 
 import com.google.common.io.LittleEndianDataInputStream
 import com.google.common.io.LittleEndianDataOutputStream
+import java.awt.image.SampleModel
 import java.io.*
 
 data class Mono16Wave(
-    val samplesPerSec: Int,
-    val bitsPerSample: Short,
-    val size: Int,
-    val data: List<Short>,
+	val samplesPerSec: Int,
+	val bitsPerSample: Short,
+	val size: Int,
+	val data: List<Short>,
 )
 
 class Mono16Processor {
-    private val blockSize
-        get() = 2
+	private val waveFormatType = 1
+	private val channel = 1
+	private val blockSize = 2
 
-    private lateinit var wave: Mono16Wave
+	private lateinit var wave: Mono16Wave
 
-    fun read(fileName: String): Mono16Processor? {
-        try {
-            LittleEndianDataInputStream(FileInputStream(fileName)).run {
-                skipNBytes(24)
-                val samplesPerSec = readInt()
-                skipNBytes(6)
-                val bitsPerSample = readShort()
-                skipNBytes(4)
-                val size = readInt()
-                val data = List<Short>(size = size / blockSize) { idx ->
-                    readShort()
-                }
-                wave = Mono16Wave(
-                    samplesPerSec, bitsPerSample, size, data
-                )
-            }
-            return this
-        } catch (e: Exception) {
-            println(e.message)
-            return null
-        }
-    }
+	fun read(fileName: String): Mono16Processor? {
+		try {
+			LittleEndianDataInputStream(FileInputStream(fileName)).run {
+				skipNBytes(24)
+				val samplesPerSec = readInt()
+				skipNBytes(6)
+				val bitsPerSample = readShort()
+				skipNBytes(4)
+				val size = readInt()
+				val data = List(size / blockSize) { readShort() }
+				wave = Mono16Wave(
+					samplesPerSec, bitsPerSample, size, data
+				)
+			}
+			return this
+		} catch (e: Exception) {
+			println(e.message)
+			return null
+		}
+	}
 
-    fun write(fileName: String): Mono16Processor? {
-        try {
-            LittleEndianDataOutputStream(FileOutputStream(File(fileName), false)).run {
-                write("RIFF".toByteArray())
-                writeInt(wave.size + 36)
-                write("WAVE".toByteArray())
-                write("fmt ".toByteArray())
-                writeInt(16)
-                writeShort(1)
-                writeShort(1)
-                writeInt(wave.samplesPerSec)
-                writeInt(wave.samplesPerSec * blockSize)
-                writeShort(blockSize)
-                writeShort(wave.bitsPerSample.toInt())
-                write("data".toByteArray())
-                writeInt(wave.size)
-                wave.data.forEach {
-                    writeShort(it.toInt())
-                }
-            }
-            return this
-        } catch (e: Exception) {
-            println(e.message)
-            return null
-        }
-    }
+	fun write(fileName: String): Mono16Processor? {
+		try {
+			LittleEndianDataOutputStream(FileOutputStream(File(fileName), false)).run {
+				write("RIFF".toByteArray())
+				writeInt(wave.size + 36)
+				write("WAVE".toByteArray())
+				write("fmt ".toByteArray())
+				writeInt(16)
+				writeShort(waveFormatType)
+				writeShort(channel)
+				writeInt(wave.samplesPerSec)
+				writeInt(wave.samplesPerSec * blockSize) // bytesPerSec
+				writeShort(blockSize)
+				writeShort(wave.bitsPerSample.toInt())
+				write("data".toByteArray())
+				writeInt(wave.size)
+				wave.data.forEach { writeShort(it.toInt()) }
+			}
+			return this
+		} catch (e: Exception) {
+			println(e.message)
+			return null
+		}
+	}
+
+	fun showInfo(): Mono16Processor {
+		if (this::wave.isInitialized) {
+			println(
+				"""
+			WaveFormatType: $waveFormatType
+			channel		  : $channel
+			SamplePerSec  : ${wave.samplesPerSec}
+			BytesPerSec   : ${wave.samplesPerSec * blockSize}
+			BlockSize     : ${blockSize}
+			BitsPerSample : ${wave.bitsPerSample}
+			DataSize      : ${wave.data.size}
+            	""".trimIndent()
+			)
+		} else {
+			println("wave is not initialized")
+		}
+		return this
+	}
 }
